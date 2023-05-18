88471b1a9d1d48d2d5966095b3e42f3a10c5801c
RedBandana <aurelende@duck.com>
2023-05-18 16:35:43 -0400

    Revert "Trying webpack"
    This reverts commit b4f7c6df7ad4aeba955b657dffe046d34ffdd77f.

--- src/utils/FileStream.js
+++ src/utils/FileStream.js
@@ -1,30 +1,7 @@
-import crypto from 'crypto-browserify';
-
-const algorithm = 'aes-256-ctr';
-const secretKey = 'mE2ggXIIpZn9MPO1iIln3X0ywajtI1Y=';
-const iv = crypto.randomBytes(16);
-
-const encryptJson = (jsonObj) => {
-  const jsonString = JSON.stringify(jsonObj);
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-  let encrypted = cipher.update(jsonString, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return {
-    iv: iv.toString('hex'),
-    content: encrypted,
-  };
-};
-
-const decryptJson = (encryptedJson) => {
-  const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(encryptedJson.iv, 'hex'));
-  let decrypted = decipher.update(encryptedJson.content, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return JSON.parse(decrypted);
-};
 
 const saveJSONToFile = (jsonObject, filename) => {
-    const data = encryptJson(jsonObject)
-    const blob = new Blob([data], { type: 'application/octet-stream' });
+    const data = JSON.stringify(jsonObject);
+    const blob = new Blob([data], { type: 'application/json' });
     const url = window.URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
@@ -47,9 +24,7 @@ const readJSONFromUserInput = (inputFile) => {
 
         fileReader.onload = () => {
             try {
-                const encryptedData = fileReader.result;
-                const decryptedData = decryptJson(encryptedData);
-                const jsonObject = JSON.parse(decryptedData);
+                const jsonObject = JSON.parse(fileReader.result);
                 resolve(jsonObject);
             } catch (error) {
                 reject(error);
@@ -76,4 +51,4 @@ const readJSONFromFile = (filename) => {
     });
 }
 
-export { saveJSONToFile, readJSONFromFile, readJSONFromUserInput, encryptJson, decryptJson }
+export { saveJSONToFile, readJSONFromFile, readJSONFromUserInput }
