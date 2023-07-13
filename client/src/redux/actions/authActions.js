37addfc4d7aa0232bab41885dc72baf40bc412b3
RedBandana <aurelende@duck.com>
2023-07-13 17:23:47 -0400

    redux-saga doesn't persist data on reload

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,6 +1,5 @@
 import { createAction } from 'redux-actions';
 
 export const loginRequest = createAction('LOGIN_REQUEST');
-export const loginRequestSent = createAction('LOGIN_REQUEST_SENT');
 export const loginSuccess = createAction('LOGIN_SUCCESS');
 export const loginFailure = createAction('LOGIN_FAILURE');
