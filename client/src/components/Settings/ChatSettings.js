157e178669f7d00914c6c314904879ce55fc5f2a
RedBandana <aurelende@duck.com>
2023-11-10 16:21:50 -0500

    removing vision preview

--- client/src/components/Settings/ChatSettings.js
+++ client/src/components/Settings/ChatSettings.js
@@ -352,7 +352,6 @@ const Settings = () => {
                                     value={formSettings.model}
                                     onChange={handleModelChange}>
                                     <option value="gpt-4-1106-preview">gpt-4 turbo</option>
-                                    <option value="gpt-4-vision-preview">gpt-4 turbo 👀</option>
                                     <option value="gpt-4">gpt-4</option>
                                     <option value="gpt-4-32k">gpt-4 32k</option>
                                     <option value="gpt-3.5-turbo">gpt-3.5 turbo</option>
